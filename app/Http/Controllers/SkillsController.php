d7a5a96e8509343e83f111923a4d430c47cab6c0
hengthay <laovkimhengthai@gmail.com>
2026-01-03 21:22:01 +0700

    Implement on SkillsController

--- app/Http/Controllers/SkillsController.php
+++ app/Http/Controllers/SkillsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SkillRequest;
 use App\Models\Skills;
 use Illuminate\Http\Request;
 
@@ -13,23 +14,71 @@ class SkillsController extends Controller
 
             return $this->handleResponse($getAllSkills, "Skill is successfully received!");
         } catch (\Throwable $e) {
-            return $this->handleErrorResponse(null, $e->getMessage(), 404);
+            return $this->handleErrorResponse(null, $e->getMessage(), 500);
         }
     }
 
     public function show($id) {
+        try {
+            $skill = Skills::findOrFail($id);
 
+            if(!$skill) {
+                return $this->handleErrorResponse(null, 'Skill is not found', 404);
+            }
+
+            return $this->handleResponse($skill, 'Skill with id: ' . $id . ' received!', 200);
+        } catch (\Throwable $e) {
+            return $this->handleErrorResponse(null, $e->getMessage(), 500);
+        }
     }
 
-    public function create(Request $request) {
+    public function create(SkillRequest $request) {
+        try {
+            $data = $request->validated();
+            
+            $skill = Skills::create($data);
 
+            if(!$skill) {
+                return $this->handleErrorResponse(null, 'Failed to create skill', 404);
+            }
+
+            return $this->handleResponse($skill, 'Skill is successfully created!', 201);
+        } catch (\Throwable $e) {
+            return $this->handleErrorResponse(null, $e->getMessage(), 500);
+        }
     }
 
-    public function update(Request $request, $id) {
+    public function update(SkillRequest $request, $id) {
+        try {
+            $skill = Skills::findOrFail($id);
+            
+            $data = $request->validated();
 
+            $skill->update($data);
+            
+            if(!$skill) {
+                return $this->handleErrorResponse(null, 'Failed to update skill', 404);
+            }
+
+            return $this->handleResponse($skill, 'Skill is successfully updated!', 201);
+        } catch (\Throwable $e) {
+            return $this->handleErrorResponse(null, $e->getMessage(), 500);
+        }
     }
 
     public function delete($id) {
-        
+        try {
+            $skill = Skills::findOrFail($id);
+            
+            if(!$skill) {
+                return $this->handleErrorResponse(null, 'Skill is not found for deleted', 404);
+            }
+
+            $skill->delete();
+
+            return $this->handleResponse(null, 'Skill is successfully deleted!', 204);
+        } catch (\Throwable $e) {
+            return $this->handleErrorResponse(null, $e->getMessage(), 500);
+        }
     }
 }
