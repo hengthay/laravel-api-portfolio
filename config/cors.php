f66721667df41ddf1dbd5782265d171f0357a84f
hengthay <laovkimhengthai@gmail.com>
2026-05-13 14:37:12 +0700

    To allowed dashboard url for access resource

--- config/cors.php
+++ config/cors.php
@@ -10,7 +10,9 @@ return [
     'http://localhost:5173', 
     'http://127.0.0.1:5173', 
     'http://localhost:5174', 
-    'http://127.0.0.1:5174'
+    'http://127.0.0.1:5174',
+    'https://laov-kimhengthay.vercel.app',
+    'https://laov-kimhengthay-dashboard.vercel.app'
   ],
 
   'allowed_origins_patterns' => [],
